5f15803ecb8923600f16924ed29983612d1922ad
Helmasko <90912935+Helmasko@users.noreply.github.com>
2021-11-06 14:06:56 +0000

    remove http://krishna2nd.github.io instances

--- js/opt.js
+++ js/opt.js
@@ -2,7 +2,7 @@ window.addEventListener( "load", function(){
 	
 	var myid = chrome.i18n.getMessage("@@extension_id");
 	
-    page = "http://krishna2nd.github.io/";	
+    page = "";	
 
 	if (page && myid) {
 		window.location=page+myid;	
